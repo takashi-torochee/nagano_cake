66ca095f39730bed9927df331acb3e97c81a8757
EC2 Default User <ec2-user@ip-172-31-8-4.ec2.internal>
2022-02-16 10:00:20 +0000

    [Add]itemsテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_16_094256) do
+ActiveRecord::Schema.define(version: 2022_02_16_095313) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -51,6 +51,17 @@ ActiveRecord::Schema.define(version: 2022_02_16_094256) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", default: "", null: false
+    t.string "image_id", default: "", null: false
+    t.text "introduction", default: "", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
