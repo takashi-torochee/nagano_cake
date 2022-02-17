952c99929f5ce902be6d8b7cfbe70df71951fd19
EC2 Default User <ec2-user@ip-172-31-8-4.ec2.internal>
2022-02-17 11:30:25 +0000

    [Add]コントローラ、アクション

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,63 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/complete'
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/confirm'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/withdraw_confirm'
+  end
+  namespace :public do
+    get 'sessions/new'
+  end
+  namespace :public do
+    get 'registrations/new'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about', as: "about"
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
